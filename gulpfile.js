8592a1dd7b631e329ab818dd1ebca63cdb2ea820
doot0 <doot0@doot0.co.uk>
2016-07-17 19:25:11 +0800

    Make gulp handle all the build related stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,43 @@
-var gulp = require('gulp'),
-    livereload = require('gulp-livereload'),
-    exec = require('child_process').exec;
+var gulp        = require('gulp'),
+    livereload  = require('gulp-livereload'),
+    sass        = require('gulp-sass'),
+    connect     = require('gulp-connect'),
+    exec        = require('child_process').exec;
 
-gulp.task('docs', function() {
-  exec('kss -c ./scss/kss.json', function(err, out, stderr) {})
+var paths = {
+  'src' : {
+    'scss' : './scss/app.scss'
+  },
+  'dist' : {
+    'scss' : './html/'
+  }
+};
 
-  gulp.src('./scss/**/*')
-      .pipe(livereload())
-});
-
-gulp.task('watch', function(){
-  livereload.listen()
-  gulp.watch('./scss/**/*', ['docs']);
+gulp.task('connect', () => {
+  connect.server({
+    root: "./",
+    livereload: true,
+    port: 8002
+  })
 })
+
+gulp.task('docs', () => {
+  exec('kss -c ./scss/kss.json', function(err, out, stderr) {})
+})
+
+gulp.task('scss', () => {
+  gulp.src(paths.src.scss)
+    .pipe(sass())
+    .pipe(gulp.dest(paths.dist.scss))
+})
+
+gulp.task('dev', ['docs', 'scss'], () => {
+  gulp.src(paths.src.scss)
+      .pipe(connect.reload())
+})
+
+gulp.task('watch', () => {
+  gulp.watch('./scss/**/*', ['dev']);
+})
+
+gulp.task('default', ['connect', 'watch'], () => {});
